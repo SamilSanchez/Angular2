9100342592f7107c87d39d3edf6cf12586f6de44
ssanchez <ssanchez@transamovil.com>
2018-02-22 16:54:34 -0400

    subiendo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: './app.component.html',
+  styleUrls:['./app.component.css']
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+
+  eventoClick():void {
+    console.log("Hola estoy escuchando")
+    this.name = this.name.toUpperCase()
+  }
+ }
